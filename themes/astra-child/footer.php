582686bbdaf8b85b221ba7e18def620c9f29e1f1
Majda1301 <149335658+Majda1301@users.noreply.github.com>
2024-06-17 23:23:59 +0200

    developpementpages

--- themes/astra-child/footer.php
+++ themes/astra-child/footer.php
@@ -13,15 +13,24 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
 ?>
 <?php astra_content_bottom(); ?>
 	</div> <!-- ast-container -->
 	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
 	</div><!-- #page -->
 <?php 
-   wp_nav_menu([
-        'theme_location'=> 'footer'
-	])
-	?>
+	astra_body_bottom();    
+	wp_footer(); 
+?>
 	</body>
 </html>
